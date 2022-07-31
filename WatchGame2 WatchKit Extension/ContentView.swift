c1fe49c8bce8dc45beeeb29e5725ab54cfa9a081
Capitaller <130102anton@gmail.com>
2022-07-31 14:18:23 +0300

    Initial Commit

---
+++ WatchGame2 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchGame2 WatchKit Extension
+//
+//  Created by Anton Shcherbakov on 31.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchGame2 WatchKit Extension/WatchGame2App.swift
@@ -0,0 +1,19 @@
+//
+//  WatchGame2App.swift
+//  WatchGame2 WatchKit Extension
+//
+//  Created by Anton Shcherbakov on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WatchGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
